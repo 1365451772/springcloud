aef1700e940a34464c53ad6d2411897fdd426708
sunpeng <1365451772@qq.com>
2021-02-09 17:51:45 +0800

    微服务

--- cloud-consumer-order80/src/main/java/com/peng/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/peng/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.peng.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,8 +13,10 @@ import org.springframework.web.client.RestTemplate;
  */
 @Configuration
 public class ApplicationContextConfig {
+
   @Bean
-  public RestTemplate getRestTemplate(){
+  @LoadBalanced
+  public RestTemplate getRestTemplate() {
     return new RestTemplate();
   }
 }
