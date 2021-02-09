6629ac896e0b67c3a64f381b7ad7d54e8bf1b432
sunpeng <1365451772@qq.com>
2021-02-09 16:43:41 +0800

    微服务

--- cloud-eureka-server7001/src/main/java/com/peng/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/peng/springcloud/EurekaMain7001.java
@@ -2,19 +2,19 @@ package com.peng.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author sp
  * @Description
- * @create 2021-02-09 11:26
+ * @create 2021-02-09 16:24
  * @Modified By:
  */
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class PaymentMain8001 {
+public class EurekaMain7001 {
 
   public static void main(String[] args) {
-    SpringApplication.run(PaymentMain8001.class,args);
+    SpringApplication.run(EurekaMain7001.class,args);
   }
 }
