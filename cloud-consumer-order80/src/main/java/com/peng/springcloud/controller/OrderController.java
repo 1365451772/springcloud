20a5205ff0e4923508d4f67ab2b7b8a1ab419c4a
sunpeng <1365451772@qq.com>
2021-02-09 17:41:46 +0800

    微服务

--- cloud-consumer-order80/src/main/java/com/peng/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/peng/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.peng.springcloud.entities.CommonResult;
 import com.peng.springcloud.entities.Payment;
 import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,16 +20,23 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class OrderController {
 
-  public static final String PAYMENT_URL = "http://localhost:8001";
+  //public static final String PAYMENT_URL = "http://localhost:8001";
+
+  public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+
   @Resource
   private RestTemplate restTemplate;
 
+//  @Resource
+//  private LoadBalancer loadBalancer;
+//  @Resource
+//  private DiscoveryClient discoveryClient;
 
-  @GetMapping ("/consumer/payment/create")
-  public CommonResult<Payment> create(Payment payment){
+  @GetMapping("/consumer/payment/create")
+  public CommonResult<Payment> create(Payment payment)
+  {
     return restTemplate.postForObject(PAYMENT_URL +"/payment/create",payment,CommonResult.class);
   }
-
 
   @GetMapping("/consumer/payment/get/{id}")
   public CommonResult<Payment> getPayment(@PathVariable("id") Long id)
@@ -36,4 +44,40 @@ public class OrderController {
     return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
   }
 
+  @GetMapping("/consumer/payment/getForEntity/{id}")
+  public CommonResult<Payment> getPayment2(@PathVariable("id") Long id)
+  {
+    ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+
+    if(entity.getStatusCode().is2xxSuccessful()){
+      return entity.getBody();
+    }else{
+      return new CommonResult<>(444,"操作失败");
+    }
+  }
+//
+//  @GetMapping(value = "/consumer/payment/lb")
+//  public String getPaymentLB()
+//  {
+//    List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+//
+//    if(instances == null || instances.size() <= 0)
+//    {
+//      return null;
+//    }
+//
+//    ServiceInstance serviceInstance = loadBalancer.instances(instances);
+//    URI uri = serviceInstance.getUri();
+//
+//    return restTemplate.getForObject(uri+"/payment/lb",String.class);
+//
+//  }
+//
+//  // ====================> zipkin+sleuth
+//  @GetMapping("/consumer/payment/zipkin")
+//  public String paymentZipkin()
+//  {
+//    String result = restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/", String.class);
+//    return result;
+//  }
 }
