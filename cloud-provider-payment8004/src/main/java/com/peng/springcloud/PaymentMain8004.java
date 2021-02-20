f71e59e7df3c3f7cd229e4ba5b171e3dffda8ec6
sunpeng <1365451772@qq.com>
2021-02-20 14:30:42 +0800

    微服务

---
+++ cloud-provider-payment8004/src/main/java/com/peng/springcloud/PaymentMain8004.java
@@ -0,0 +1,21 @@
+package com.peng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author sp
+ * @Description
+ * @create 2021-02-20 14:02
+ * @Modified By:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient//给注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentMain8004.class,args);
+  }
+
+}
