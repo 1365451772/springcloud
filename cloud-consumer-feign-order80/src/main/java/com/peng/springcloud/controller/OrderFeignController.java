82bc4ddf5792befbb4134fed1b3dc577b3ef88bf
sunpeng <1365451772@qq.com>
2021-02-22 11:19:24 +0800

    微服务

--- cloud-consumer-feign-order80/src/main/java/com/peng/springcloud/controller/OrderFeignController.java
+++ cloud-consumer-feign-order80/src/main/java/com/peng/springcloud/controller/OrderFeignController.java
@@ -1,8 +1,12 @@
 package com.peng.springcloud.controller;
 
+import com.peng.springcloud.entities.CommonResult;
+import com.peng.springcloud.entities.Payment;
 import com.peng.springcloud.service.PaymentFeignService;
 import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,4 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderFeignController {
   @Resource
   private PaymentFeignService paymentFeignService;
+
+  @GetMapping(value = "/consumer/payment/get/{id}")
+  public CommonResult<Payment> getPaymentById(@PathVariable("id")Long id){
+    return paymentFeignService.getPaymentById(id);
+  }
 }
